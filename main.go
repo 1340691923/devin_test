3a5b8d7e38f9ca58fcc60998cae802d98b527e0b
肖文 <38397940+1340691923@users.noreply.github.com>
2025-05-22 10:04:47 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"ev-plugin/backend/migrate"
+	"ev-plugin/backend/router"
+	"ev-plugin/frontend"
+	"flag"
+	"github.com/1340691923/eve-plugin-sdk-go/backend/plugin_server"
+	"github.com/1340691923/eve-plugin-sdk-go/build"
+)
+
+//go:embed plugin.json
+var pluginJsonBytes []byte
+
+func main() {
+
+	flag.Parse()
+
+	plugin_server.Serve(plugin_server.ServeOpts{
+		Assets: &plugin_server.Assets{
+			PluginJsonBytes: pluginJsonBytes,
+			FrontendFiles:   frontend.StatisFs,
+		},
+		ReadyCallBack: func(ctx context.Context) {
+		},
+		Migration: &build.Gormigrate{Migrations: []*build.Migration{
+			migrate.V0_0_1(),
+		}}, //数据版本迁移
+		RegisterRoutes: router.NewRouter,
+	})
+}
